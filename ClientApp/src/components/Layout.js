542b948794c9bf8d53fe8bc10818ef9e7223eb7b
jsBear92 <js104427@gmail.com>
2023-01-11 19:20:15 +0800

    Modify Navbar

--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -1,25 +1,22 @@
 import React from "react";
-import { Outlet, Link } from "react-router-dom";
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
     return (
         <>
-            <nav className="navbar navbar-default">
-                <div className="container-fluid">
-                    <div className="navbar-header">
-                        <Link to="/" className="navbar-brand">Rotto</Link>
-                    </div>
-                    <ul className="nav navbar-nav">
-                        <li>
-                            <Link to="/todo">Todo</Link>
-                        </li>
-                        <li>
-                            <Link to="/map">Map</Link>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="/">Rotto</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="todo">Todo</Nav.Link>
+                            <Nav.Link href="map">Map</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
             <Outlet />
         </>
     )
