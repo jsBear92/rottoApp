ab972a9ed68ea5cae36aa66e0b8b20ac74dc81b8
jsBear92 <js104427@gmail.com>
2023-01-18 16:01:05 +0800

    add datepicker

--- ClientApp/src/components/Modal/ModalContent.js
+++ ClientApp/src/components/Modal/ModalContent.js
@@ -1,7 +1,12 @@
 import { useState } from 'react';
 import { Button, Modal } from 'react-bootstrap';
+import DatePicker from 'react-datepicker';
+
+import "react-datepicker/dist/react-datepicker.css";
 
 const ModalContent = ({data, onClick, changeValue, deleteTodo}) => {
+
+    const [startDate, setStartDate] = useState(new Date());
 
     const [inputValues, setInputValues] = useState({
         title: '',
@@ -31,20 +36,35 @@ const ModalContent = ({data, onClick, changeValue, deleteTodo}) => {
         onClick();
     }
 
+    const pressEnter = (e, index) => {
+        if (e.key === "Enter") {
+            handleClick(index);
+        }
+    }
+
+    const test = () => {
+        console.log(Date.parse(data.dueDate));
+        console.log(startDate);
+        console.log(Date.parse(startDate));
+    }
+    
+
 
     return (
         <>
             <Modal.Header closeButton>
-                <Modal.Title><input defaultValue={data.title} onChange={(e) => handleTitle(e)} /></Modal.Title>
+                <Modal.Title><input className="modal-input" defaultValue={data.title} onKeyDown={(e) => pressEnter(e, data.todoId)} onChange={(e) => handleTitle(e)} /></Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <input defaultValue={data.content} onChange={(e) => handleContent(e)} />
-                <input defaultValue={data.dueDate} onChange={(e) => handleDate(e)} />
+                <input className="modal-input" defaultValue={data.content} onKeyDown={(e) => pressEnter(e, data.todoId)} onChange={(e) => handleContent(e)} />
+                <DatePicker className="modal-input" dateFormat={'yyyy-MM-dd'} selected={startDate} onChange={(date) => setStartDate(date)} />
+                <input className="modal-input" defaultValue={data.dueDate} onKeyDown={(e) => pressEnter(e, data.todoId)} onChange={(e) => handleDate(e)} />
             </Modal.Body>
             <Modal.Footer>
                 <Button variant='danger' onClick={() => deleteClick(data.todoId)} >Delete</Button>
                 <Button variant='primary' onClick={() => handleClick(data.todoId)}>Change</Button>
                 <Button variant='secondary' onClick={onClick}>Close</Button>
+                <button onClick={test}></button>
             </Modal.Footer>
         </>
     );
